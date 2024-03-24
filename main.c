785c625c5a74f69b2c5310ea68a08e8b7cf209a7
sudharsanmuthuvadivel <100138737+sudharsanmuthuvadivel@users.noreply.github.com>
2024-03-24 09:27:40 +0530

    add Embedded-c master

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 21, 2023
+ *      Author: sudh1
+ */
+#include<stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n");
+	printf("Press Enter to exit this application\n");
+	getchar();
+	return 0;
+}
+
